faf33422faabc75550c4cc01334e3f7343ae11ab
João L. Barboza <105928845+joaolucasbarboza@users.noreply.github.com>
2024-09-29 00:57:01 -0300

    feat: criar o domain `Cult`

---
+++ src/main/java/br/com/ipr/domain/entities/cult/Cult.java
@@ -0,0 +1,50 @@
+package br.com.ipr.domain.entities.cult;
+
+import java.util.Date;
+
+public class Cult {
+
+    private int id;
+    private String title;
+    private String description;
+    private Date date;
+
+    public Cult(int id, String title, String description, Date date) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
