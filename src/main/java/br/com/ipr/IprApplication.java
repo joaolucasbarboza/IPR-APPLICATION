759daeb4151d7a3e305ab62db91260dffc8d43c3
João L. Barboza <105928845+joaolucasbarboza@users.noreply.github.com>
2024-09-29 00:39:18 -0300

    Commit inicial

---
+++ src/main/java/br/com/ipr/IprApplication.java
@@ -0,0 +1,13 @@
+package br.com.ipr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ipr/IprApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ipr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
