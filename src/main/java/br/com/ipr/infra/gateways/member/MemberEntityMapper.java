7af743c9e26d444a6ebf43411782fc93ee4bd3ea
João L. Barboza <105928845+joaolucasbarboza@users.noreply.github.com>
2024-10-01 20:57:00 -0300

    feat: classe de conversão de entidade para dominio e de dominio para entidade

---
+++ src/main/java/br/com/ipr/infra/gateways/member/MemberEntityMapper.java
@@ -0,0 +1,31 @@
+package br.com.ipr.infra.gateways.member;
+
+import br.com.ipr.domain.entities.member.Member;
+import br.com.ipr.infra.persistence.member.MemberEntity;
+
+public class MemberEntityMapper {
+
+    public MemberEntity toEntity(Member member) {
+        return new MemberEntity(
+                member.getCpf(),
+                member.getName(),
+                member.getEmail(),
+                member.getPassword(),
+                member.getTelephone(),
+                member.getGender(),
+                member.getBirth()
+        );
+    }
+
+    public Member toDomain(MemberEntity memberEntity) {
+        return new Member(
+                memberEntity.getCpf(),
+                memberEntity.getName(),
+                memberEntity.getEmail(),
+                memberEntity.getPassword(),
+                memberEntity.getTelephone(),
+                memberEntity.getGender(),
+                memberEntity.getBirth()
+        );
+    }
+}
