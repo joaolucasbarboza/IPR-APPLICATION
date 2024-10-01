914f061035b3fd49c3acba45dde76f265c905ec8
João L. Barboza <105928845+joaolucasbarboza@users.noreply.github.com>
2024-10-01 20:54:04 -0300

    feat: adicionar a classe de entidade do `Member`

---
+++ src/main/java/br/com/ipr/infra/persistence/member/MemberEntity.java
@@ -0,0 +1,54 @@
+package br.com.ipr.infra.persistence.member;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@Table(name = "member")
+@Getter
+@Setter
+public class MemberEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    private String cpf;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String email;
+
+    private String password;
+    private String telephone;
+    private String gender;
+    private LocalDate birth;
+    private LocalDate dateRegistered;
+
+    public MemberEntity() {}
+
+    public MemberEntity(String cpf, String name, String email, String password, String telephone, String gender, LocalDate birth) {
+        this.cpf = cpf;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.telephone = telephone;
+        this.gender = gender;
+        this.birth = birth;
+        this.dateRegistered = LocalDate.now();
+    }
+}
