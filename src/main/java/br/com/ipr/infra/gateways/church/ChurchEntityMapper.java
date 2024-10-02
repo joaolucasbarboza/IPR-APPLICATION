a48c77d129195d7cccd7c08e9c0f1eb8a1882313
João L. Barboza <105928845+joaolucasbarboza@users.noreply.github.com>
2024-10-02 00:24:26 -0300

    feat: adicionar classe de conversão de entidade para dominio e de dominio para entidade

---
+++ src/main/java/br/com/ipr/infra/gateways/church/ChurchEntityMapper.java
@@ -0,0 +1,27 @@
+package br.com.ipr.infra.gateways.church;
+
+import br.com.ipr.domain.entities.church.Church;
+import br.com.ipr.infra.persistence.church.ChurchEntity;
+
+public class ChurchEntityMapper {
+
+    public ChurchEntity toEntity(Church church) {
+        return new ChurchEntity(
+                church.getId(),
+                church.getName(),
+                church.getDescription(),
+                church.getImage(),
+                church.getIdShepherd()
+        );
+    }
+
+    public Church toDomain(ChurchEntity churchEntity) {
+        return new Church(
+                churchEntity.getId(),
+                churchEntity.getName(),
+                churchEntity.getDescription(),
+                churchEntity.getImage(),
+                churchEntity.getIdShepherd()
+        );
+    }
+}
