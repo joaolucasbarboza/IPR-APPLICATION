e62214c998c75d4d2a1667aff4bb9f386152003b
João L. Barboza <105928845+joaolucasbarboza@users.noreply.github.com>
2024-10-01 21:04:19 -0300

    feat: adicionando o `Bean`de configuração do Swagger

---
+++ src/main/java/br/com/ipr/infra/springdoc/SpringDocConfig.java
@@ -0,0 +1,17 @@
+package br.com.ipr.infra.springdoc;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("IPR APPLICATION"));
+    }
+}
