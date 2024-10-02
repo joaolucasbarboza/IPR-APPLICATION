0dea84b42659da26006bc61155e37811156b6eed
João L. Barboza <105928845+joaolucasbarboza@users.noreply.github.com>
2024-10-02 00:01:53 -0300

    chore: adicionar a anotação `NotNull` e `NotBlank` em ChurchEntity

--- src/main/java/br/com/ipr/infra/persistence/church/ChurchEntity.java
+++ src/main/java/br/com/ipr/infra/persistence/church/ChurchEntity.java
@@ -1,6 +1,8 @@
 package br.com.ipr.infra.persistence.church;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,11 +18,10 @@ import java.util.UUID;
 @Setter
 public class ChurchEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private String name;
-    private String description;
-    private String image;
-    private String idShepherd;
+    @NotBlank private String name;
+    @NotBlank private String description;
+    @NotNull private String image;
+    @NotNull private UUID idShepherd;
 }
