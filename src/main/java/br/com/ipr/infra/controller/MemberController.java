57d7d248efdd166ea16587e7b7dc157f3da604f6
João L. Barboza <105928845+joaolucasbarboza@users.noreply.github.com>
2024-10-01 21:03:14 -0300

    feat: adicionando o controller e o método `PostMapping`

---
+++ src/main/java/br/com/ipr/infra/controller/MemberController.java
@@ -0,0 +1,46 @@
+package br.com.ipr.infra.controller;
+
+import br.com.ipr.application.usecases.member.CreateMember;
+import br.com.ipr.domain.entities.member.Member;
+import br.com.ipr.infra.request.MemberRequestDto;
+import br.com.ipr.infra.response.MemberResponseDto;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+public class MemberController {
+
+    private final CreateMember createMember;
+
+    public MemberController(final CreateMember createMember) {
+        this.createMember = createMember;
+    }
+
+    @PostMapping
+    @Transactional
+    public MemberResponseDto create(@RequestBody MemberRequestDto dto) {
+        Member salved = createMember.registerMember(new Member(
+                dto.cpf(),
+                dto.name(),
+                dto.email(),
+                dto.password(),
+                dto.telephone(),
+                dto.gender(),
+                dto.birth()
+        ));
+
+        return new MemberResponseDto(
+                salved.getCpf(),
+                salved.getName(),
+                salved.getEmail(),
+                salved.getTelephone(),
+                salved.getGender(),
+                salved.getBirth(),
+                salved.getDateRegistered()
+        );
+    }
+}
