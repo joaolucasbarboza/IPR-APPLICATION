2a2932e7b255115f076202b0a1de202bd5c8b461
João L. Barboza <105928845+joaolucasbarboza@users.noreply.github.com>
2024-10-02 00:29:26 -0300

    refactor: adicionar as anotações do Lombok `Getter`, `Setter` e AllArgsConstructor em Church

--- src/main/java/br/com/ipr/domain/entities/church/Church.java
+++ src/main/java/br/com/ipr/domain/entities/church/Church.java
@@ -1,7 +1,14 @@
 package br.com.ipr.domain.entities.church;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class Church {
 
     private UUID id;
@@ -10,58 +17,4 @@ public class Church {
     private String image;
     private UUID idShepherd;
 
-    public Church(String name, String description, String image, UUID idShepherd) {
-        this.name = name;
-        this.description = description;
-        this.image = image;
-        this.idShepherd = idShepherd;
-    }
-
-    public Church(UUID id, String name, String description, String image, UUID idShepherd) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.image = image;
-        this.idShepherd = idShepherd;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public UUID getIdShepherd() {
-        return idShepherd;
-    }
-
-    public void setIdShepherd(UUID idShepherd) {
-        this.idShepherd = idShepherd;
-    }
 }
