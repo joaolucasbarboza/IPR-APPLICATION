378ca5a686a7679d3e668558c1702672ed8aec46
João L. Barboza <105928845+joaolucasbarboza@users.noreply.github.com>
2024-10-02 00:30:11 -0300

    refactor: adicionar as anotações do Lombok `Getter`e `Setter` em Member

--- src/main/java/br/com/ipr/domain/entities/member/Member.java
+++ src/main/java/br/com/ipr/domain/entities/member/Member.java
@@ -1,9 +1,13 @@
 package br.com.ipr.domain.entities.member;
 
 import br.com.ipr.infra.exceptions.member.IncorretPatternCPF;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class Member {
     private String cpf;
     private String name;
@@ -28,69 +32,5 @@ public class Member {
         this.gender = gender;
         this.birth = birth;
         this.dateRegistered = LocalDate.now();
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public LocalDate getBirth() {
-        return birth;
-    }
-
-    public void setBirth(LocalDate birth) {
-        this.birth = birth;
-    }
-
-    public LocalDate getDateRegistered() {
-        return dateRegistered;
-    }
-
-    public void setDateRegistered(LocalDate dateRegistered) {
-        this.dateRegistered = dateRegistered;
     }
 }
