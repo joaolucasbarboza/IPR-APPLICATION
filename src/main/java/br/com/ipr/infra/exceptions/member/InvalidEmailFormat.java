a668cea73997f4098be315db95225cb54e18ac77
joaobarboza <joaolucasbarbozaa@gmail.com>
2024-10-17 19:49:25 -0300

    feat: criar exception de Email do membro inválido.

---
+++ src/main/java/br/com/ipr/infra/exceptions/member/InvalidEmailFormat.java
@@ -0,0 +1,15 @@
+package br.com.ipr.infra.exceptions.member;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class InvalidEmailFormat extends RuntimeException {
+
+    public InvalidEmailFormat(String message) {
+        super(message);
+    }
+
+    public InvalidEmailFormat() {
+        super("Invalid email format");
+    }
+}
