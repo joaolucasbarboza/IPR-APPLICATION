a16c9aa5358201c8852d95e0090a1594b8e140e2
joaobarboza <joaolucasbarbozaa@gmail.com>
2024-10-17 19:49:52 -0300

    feat: criar exception de membro já existe

---
+++ src/main/java/br/com/ipr/infra/exceptions/member/MemberAlreadyExist.java
@@ -0,0 +1,15 @@
+package br.com.ipr.infra.exceptions.member;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class MemberAlreadyExist extends RuntimeException {
+
+    public MemberAlreadyExist(String message) {
+        super(message);
+    }
+
+    public MemberAlreadyExist() {
+        super("Member already exists");
+    }
+}
