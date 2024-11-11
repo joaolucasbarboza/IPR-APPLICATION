eb413807d671b849ccfd50d2d672ab425b0d09f1
João L. Barboza <joaolucasbarbozaa@gmail.com>
2024-11-11 19:56:25 -0300

    feat: adicionar associação entre membro e igreja

--- src/main/java/br/com/ipr/infra/gateways/member/RepositoryMemberImpl.java
+++ src/main/java/br/com/ipr/infra/gateways/member/RepositoryMemberImpl.java
@@ -2,24 +2,23 @@ package br.com.ipr.infra.gateways.member;
 
 import br.com.ipr.application.gateways.RepositoryMember;
 import br.com.ipr.domain.Member;
+import br.com.ipr.infra.exceptions.member.MemberNotFound;
+import br.com.ipr.infra.gateways.church.ChurchEntityMapper;
 import br.com.ipr.infra.persistence.member.MemberEntity;
 import br.com.ipr.infra.persistence.member.MemberRepository;
 import java.util.List;
 import java.util.Optional;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class RepositoryMemberImpl implements RepositoryMember {
 
   private final MemberRepository memberRepository;
   private final MemberEntityMapper memberEntityMapper;
-
-  public RepositoryMemberImpl(
-      MemberRepository memberRepository, MemberEntityMapper memberEntityMapper) {
-    this.memberRepository = memberRepository;
-    this.memberEntityMapper = memberEntityMapper;
-  }
+  private final ChurchEntityMapper churchEntityMapper;
 
   @Override
-  public Member createMember(Member member) {
+  public Member save(Member member) {
     MemberEntity entity = memberEntityMapper.toMemberEntity(member);
     memberRepository.save(entity);
 
@@ -27,8 +26,29 @@ public class RepositoryMemberImpl implements RepositoryMember {
   }
 
   @Override
-  public boolean findByCpf(String cpf) {
+  public Member association(Member member) {
+    MemberEntity entity = memberEntityMapper.toMemberEntity(member);
+
+    entity.setChurch(churchEntityMapper.toChurchEntity(member.getChurch()));
+    memberRepository.save(entity);
+
+    return memberEntityMapper.toMemberDomain(entity);
+  }
+
+  @Override
+  public boolean existsByCpf(String cpf) {
     return memberRepository.findByCpf(cpf).isPresent();
+  }
+
+  @Override
+  public Member findByCpf(String cpf) {
+
+    MemberEntity entity =
+        memberRepository
+            .findByCpf(cpf)
+            .orElseThrow(() -> new MemberNotFound(String.format("Member CPF %s not found", cpf)));
+
+    return memberEntityMapper.toMemberDomain(entity);
   }
 
   @Override
